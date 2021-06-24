14ddb8cec9e0c1fc5c0d5ac9bcc305a82c56bb92
areckt <arkadiusz.dawid.99@gmail.com>
2021-06-24 16:03:53 +0200

    data binding

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ new Vue({
   data: {
     name: 'Arek',
     job: 'Frontend dev',
+    website: 'https://www.youtube.com',
+    websiteTag: '<a href="https://www.youtube.com">YouTube</a>',
   },
   methods: {
     greet: function (time) {
