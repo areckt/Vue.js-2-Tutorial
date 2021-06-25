0c49c13005e4b1e559736a6cd3d447bdd98d1ba0
areckt <arkadiusz.dawid.99@gmail.com>
2021-06-25 15:36:17 +0200

    two-way data binding

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {
-    logName: function () {
-      console.log('you entered your name');
-    },
-    logAge: function () {
-      console.log('you entered your age');
-    },
+  data: {
+    name: '',
+    age: '',
   },
+  methods: {},
 });
