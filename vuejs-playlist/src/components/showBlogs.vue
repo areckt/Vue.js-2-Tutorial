405ec386eada712b55384eddf22a9eb661ed5a74
areckt <arkadiusz.dawid.99@gmail.com>
2021-06-28 21:58:57 +0200

    37 - registering directives and filters locally

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div v-theme:column="'wide'" id="show-blogs">
       <h1>All Blog Articles</h1>
       <input type="text" v-model="search" placeholder="search blogs" />
       <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
@@ -35,6 +35,45 @@ export default {
               return blog.title.match(this.search);
           });
       }
+  },
+
+  filters: {
+      'to-uppercase': function(value) {
+          return value.toUpperCase();
+      },
+      'snippet': function(value) {
+        let newValue = value.slice(0, 100);
+        if (value.length > 100) {
+          newValue += "...";
+        }
+        return newValue;
+      }
+  },
+
+  directives: {
+    'rainbow': {
+      bind(el, binding, vnode) {
+				el.style.color =
+				"#" +
+				Math.random()
+					.toString()
+					.slice(2, 8);
+			}
+    },
+		'theme': {
+			bind(el, binding, vnode) {
+				if (binding.value == "wide") {
+					el.style.maxWidth = "1200px";
+				} else if (binding.value == "narrow") {
+					el.style.maxWidth = "500px";
+				}
+
+				if (binding.arg == "column") {
+					el.style.background = "#ddd";
+					el.style.padding = "20px";
+				}
+			}
+		}
   }
 }
 </script>
