1422f1b9a17415225f99d44bc19e48b9c438f2de
areckt <arkadiusz.dawid.99@gmail.com>
2021-06-29 11:20:58 +0200

    41 - adding router links

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -13,45 +13,6 @@ const router = new VueRouter({
   mode: "history"
 });
 
-// Custom directives
-// Vue.directive("rainbow", {
-//   bind(el, binding, vnode) {
-//     el.style.color =
-//       "#" +
-//       Math.random()
-//         .toString()
-//         .slice(2, 8);
-//   }
-// });
-
-// Vue.directive("theme", {
-//   bind(el, binding, vnode) {
-//     if (binding.value == "wide") {
-//       el.style.maxWidth = "1200px";
-//     } else if (binding.value == "narrow") {
-//       el.style.maxWidth = "500px";
-//     }
-
-//     if (binding.arg == "column") {
-//       el.style.background = "#ddd";
-//       el.style.padding = "20px";
-//     }
-//   }
-// });
-
-// Filters
-// Vue.filter("to-uppercase", value => {
-//   return value.toUpperCase();
-// });
-
-// Vue.filter("snippet", value => {
-//   let newValue = value.slice(0, 100);
-//   if (value.length > 100) {
-//     newValue += "...";
-//   }
-//   return newValue;
-// });
-
 new Vue({
   el: "#app",
   render: h => h(App),
